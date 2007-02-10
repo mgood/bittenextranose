3b24de72a7a268a04c914a84daf65e4b3c6ff087
s0undt3ch <s0undt3ch@46a83df0-2629-0410-9708-a544d52267ea>
2007-02-10 23:26:46 +0000

    Corrected `setup.py`, missing comma, and added the missing `__init__.py` for nosebitten.
    git-svn-id: http://bitten.ufsoft.org/svn/BittenExtraNose/trunk@3 46a83df0-2629-0410-9708-a544d52267ea

--- nosebitten/__init__.py
+++ nosebitten/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: sw=4 ts=4 fenc=utf-8
 # =============================================================================
@@ -14,21 +13,3 @@
 # Please view LICENSE for additional licensing information.
 # =============================================================================
 
-from setuptools import setup, find_packages
-
-setup(
-    name='BittenExtraNose',
-    version='0.1',
-    author='Pedro Algarvio',
-    author_email = 'ufs@ufsoft.org',
-    description = 'Bitten Nose plugins',
-    license = 'BSD',
-    packages = find_packages(),
-    install_requires=['nose>=0.9.2', 'bitten==dev,>=0.6dev-r378'],
-    entry_points = {
-        'nose.plugins': [
-            'bitten#nosetests = nosebitten.plugnose:BittenNosetests',
-            'bitten#nosecoverage = nosebitten.plugnose:BittenNoseCoverage'
-         ]
-    }
-)
